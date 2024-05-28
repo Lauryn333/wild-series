0db7f5301676414c98ef47eceae0e4bfd04cc699
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-05-28 13:01:15 +0200

    Quete 05 : Ajout des BDD category et program

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,19 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+create table category(
+ id int unsigned primary key auto_increment not null,
+ name varchar(100)
+);
+
+create table program(
+  id int unsigned primary key auto_increment not null,
+  title varchar(200) not null,
+  synopsis text not null,
+  poster varchar(200),
+  country varchar(100),
+  year int not null,
+  category_id int unsigned not null,
+  foreign key(category_id) references category(id)
+);
